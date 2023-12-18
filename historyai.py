92efbedad322748d40f94fda137b3ce555995435
Vincent Wehren <vinweh@users.noreply.github.com>
2023-12-17 19:52:44 -0800

    Fixes, update to OpenAI v 1.50, CSV fixes

--- historyai.py
+++ historyai.py
@@ -1,9 +1,8 @@
-
 import os
 import sys
-import json
 from contextlib import closing
-import openai
+
+from openai import OpenAI
 
 from historydb import HistoryDb
 from utils import num_tokens_for_message
@@ -12,28 +11,23 @@ from utils import num_tokens_for_message
 def classify(history_items):
     """Very naively classify the history items (url/title pairs) and provide confidence
     """
-    client = openai.OpenAI()
-
-    #openai.api_key = os.getenv("OPENAI_API_KEY")
-    model = os.getenv("OPENAI_MODEL")
-    print(f"Using model {model}")
-    
-   
+    client = OpenAI()
+    model = "gpt-3.5-turbo-1106"
+    temperature = 0.3
     system_content = open("./system-prompt-csv.txt").read()
-    
     messages = []
     responses = []
     num_tokens = 0
     max_tokens = 2000
 
     print(f"Rows found {len(history_items)}")
-    message = {"role": "system", "content": system_content}
-
-    num_tokens += num_tokens_for_message(message, model)
-    messages.append(message)
+    sys_message = {"role": "system", "content": system_content}
+    sys_tokens = num_tokens_for_message(sys_message, model)
+    num_tokens += sys_tokens
+    messages.append(sys_message)
     model_used = None
+
     for index, row in enumerate(history_items):
-        
          url, title = row
          content = f"URL: {url}, TITLE: {title}"
          message = {"role" : "user", "content" : content}
@@ -46,42 +40,46 @@ def classify(history_items):
             # We've reached already reached our token limit
             print(f"Token limit reached at {index}")
             # let's first get an intermediate response before moving on with this message
-            response = client.ChatCompletion.create(
+            response = client.chat.completions.create(
                      model = model
-                    ,temperature=0.6
+                    ,temperature=temperature
                     ,messages=messages
                     ,response_format={"type" : "text"}
                 )
+            
             # grab the contents of that response and append it to the responses list
-            r = response.choices[0].message['content'].strip()
+            r = response.choices[0].message.content
             responses.append(r)
-            num_tokens = num_tokens_for_message(message, model) # count from the last message that ddn't make it
-            #reset for the the next batch of messages and append
+            num_tokens = num_tokens_for_message(message, model)
+            # message queue reset for the the next batch of messages, reset sys message and append last message
             messages = []
-            messages.append({"role": "system", "content": system_content})
+            messages.append(sys_message)
+            num_tokens += sys_tokens
             messages.append(message)
   
     # no more messages to append
-    messages.append({"role": "user", "content" : "ALL SENT"})
+    all_sent_message = ({"role": "user", "content" : "ALL SENT"})
+    num_tokens += num_tokens_for_message(all_sent_message, model)
+    messages.append(all_sent_message)
     # get the last response (which is also the first if token limit was never reached)
-    response = client.ChatCompletion.create(
+    response = client.chat.completions.create(
                     model = model
-                    ,temperature=0.6
+                    ,temperature=temperature
                     ,messages=messages
                     ,response_format={"type" : "text"}
                 )
- 
     
     if not model_used:
         model_used = response.model
         print(f"Model actually used: {model_used}")
+    
     # add the content         
-    final_response = response.choices[0].message["content"].strip()
-    responses.append(final_response)
+    final_response = response.choices[0].message.content
+    responses.append(final_response)   
     return responses
 
 def get_urls(limit):
-    """Get the URL, Tile pairs from the database"""
+    """Get the URL, Title pairs from the history database"""
     
     browser_profile_path  = os.getenv("EDGE_BROWSER_PROFILE_PATH")
     history_db = os.path.join(os.environ['HOME'], browser_profile_path, "History")
@@ -93,17 +91,19 @@ def get_urls(limit):
 
    
 def main():
+    """The main train"""
     sep = ","
     header = ["\"Title\"","\"Url\"", "\"Category\"","\"Confidence Level\""]
-    urls = get_urls(limit=50) #increase limit to comfort level
+    urls = get_urls(limit=100) #decrease or increase limit to comfort level
     if urls:
        # write predictions to csv file, do something smarter later
-        predictions = classify(urls)
-        
+        predictions = classify(urls)        
         with open("classifications.csv", "w") as f:
             f.write(sep.join(header))
             f.write("\n")    
             for idx, p in enumerate(predictions):
+                if not p.endswith("\n"):
+                    p = "".join([p, "\n"])
                 f.write(p)
                 
     else:
